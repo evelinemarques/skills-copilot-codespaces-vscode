04f951af33627bcad7ac402c933c5ba4c354bebf
Eveline Marques Guimarães <evelineguimaraes@inf.ufg.br>
2023-07-17 19:00:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,2 @@
-//Create a web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
+// Create web server
 
